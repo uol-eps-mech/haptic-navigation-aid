71e12b5689425d1df2de7aece6d632789d5e47b1
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-02-07 12:08:07 +0000

    Add Raspberry Pi File

---
+++ hapticInputRpi.py
@@ -0,0 +1,155 @@
+import time
+import random
+import json
+from gpiozero import Button, LED
+import time
+
+debounce : int = 200
+record_count  : int  = 0
+sequence = [100 for _ in range(5)]
+
+buttonPins : list[int] = [24]
+buttons = []
+
+ledPins : list[int] = [23]
+leds = []
+
+switchPin = 10
+# switch = board.digital[switchPin]
+
+def setup():
+    # switch = board.digital[switchPin]
+    # switch.mode = pyfirmata.INPUT
+    # GPIO.setup(18,GPIO.OUT)
+
+    for buttonPin in buttonPins:
+        button = Button(buttonPin)
+        buttons.append(button)
+
+    for ledPin in ledPins:
+        led = LED(ledPin)
+        leds.append(led)
+
+def get_input_states():
+    buttonStates = get_buttons_state()
+    switchState = True
+    return buttonStates, switchState
+
+def get_buttons_state():
+    button1 = buttons[0].is_pressed
+    button2 = buttons[1].is_pressed
+    button3 = buttons[2].is_pressed
+    button4 = buttons[3].is_pressed
+    return [button1, button2, button3, button4]
+
+def turn_off_leds():
+    for led in leds:
+        led.value = 0
+
+def turn_on_leds():
+    for led in leds:
+        led.value = 1
+
+def flash_leds():
+    for _ in range(4):
+        turn_off_leds()
+        time.sleep(0.2)
+        turn_on_leds()
+        time.sleep(0.2)
+        turn_off_leds()
+        time.sleep(0.5)
+    
+
+def print_sequence(sequence):
+    print("------------ CURRENT SEQEUENCE ----------------")
+    print(sequence)
+    print("-----------------------------------------------")
+
+def play_sequence(sequence):
+    for led in sequence:
+        leds[led].value = 1
+        time.sleep(0.5)
+        leds[led].value = 1
+        time.sleep(0.5)
+
+def get_closest_anchor():
+    anchors = ["0x111", "0x222", "0x333", "0x444", "0x555"]
+    index = random.randint(0,4)
+    return anchors[index]
+
+def addOrUpdateAnchorMapping(anchor, sequence):
+    jsonFile = open("store.json", "r")
+    mappings = json.load(jsonFile)
+    jsonFile.close()
+
+    try:
+        mappings.update({anchor: sequence})
+    except:
+        mappings[anchor] = sequence
+
+    jsonFile = open("store.json", "w")
+    jsonFile.write(json.dumps(mappings, sort_keys=True))
+    jsonFile.close()
+
+def wait_for_location_request(buttonStates):
+    # print("switch off", buttonStates, switchOn)
+    if (any(buttonStates)):
+        closest_anchor = get_closest_anchor()
+        jsonFile = open("store.json", "r")
+        mappings = json.load(jsonFile)
+        jsonFile.close()
+
+        sequence_to_play = [100, 100, 100, 100, 100]
+
+        try:
+            sequence_to_play = mappings[closest_anchor]
+        except:
+            pass
+
+        if (any(x != 100 for x in sequence_to_play)):
+            time.sleep(0.7)
+            print_sequence(sequence_to_play)
+            play_sequence(sequence_to_play)
+    
+
+def record_sequence(buttonStates, counter):
+    # print("switch on", buttonStates, switchOn)
+    if counter < 5:
+        if (any(buttonStates)):
+            print("Counter: ", counter)
+            for id, state in enumerate(buttonStates):
+                if state == 1:
+                    led = LED(ledPins[id])
+                    led.value = 1
+                    sequence[counter] = (id)
+                    time.sleep(0.5)
+                    turn_off_leds()
+                    print("Button" , id+1, "pressed")
+                    print("Current Sequence:", sequence)
+            return counter + 1
+    else:
+        closest_anchor = get_closest_anchor()
+        addOrUpdateAnchorMapping(closest_anchor, sequence)
+        time.sleep(0.5)
+        flash_leds()
+        return 0
+
+    return counter
+
+while True:
+    setup()
+    time.sleep(0.4)
+    buttonStates, switchOn = get_input_states()
+    
+    print("here")
+    counter = 0
+    while(switchOn):
+        buttonStates, switchOn = get_input_states()
+        if(not switchOn): break
+        counter = record_sequence(buttonStates, counter)
+
+    while(not switchOn):
+        buttonStates, switchOn = get_input_states()
+        if(switchOn): break
+
+        wait_for_location_request(buttonStates)
