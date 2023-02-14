afd3126dd20af527599d9d0af8586b937f5ecfc9
asfhja <easfa>
2023-02-14 10:55:22 +0000

    Add function to indicate distance to closest anchor when user requests

--- Haptic Input/haptic_input.py
+++ Haptic Input/haptic_input.py
@@ -19,14 +19,14 @@ def get_input_states():
     switch_state = MCU.get_switch_state(switch_pin)
     return button_states, switch_state
 
-def flash_leds():
-    for _ in range(4):
+def flash_leds(delay, no_of_flashes=4):
+    for _ in range(no_of_flashes):
         MCU.turn_off_leds()
-        time.sleep(0.2)
+        time.sleep(delay/2)
         MCU.turn_on_leds()
-        time.sleep(0.2)
+        time.sleep(delay)
         MCU.turn_off_leds()
-        time.sleep(0.5)
+        time.sleep(delay/2)
 
 def print_sequence(sequence):
     print("------------ CURRENT SEQEUENCE ----------------")
@@ -40,10 +40,22 @@ def play_sequence(sequence):
         MCU.turn_off_led(led_pins[led])
         time.sleep(0.5)
 
+anchors = ["0x111", "0x222", "0x333", "0x444", "0x555"]
 def get_closest_anchor():
-    anchors = ["0x111", "0x222", "0x333", "0x444", "0x555"]
     index = random.randint(0,4)
     return anchors[index]
+
+def get_anchor_position(anchor_id):
+    return (random.randint(0,12), random.randint(0,12))
+
+def get_user_position():
+    return (random.randint(0,12), random.randint(0,12))
+
+def get_distance_from_anchor(anchor_id):
+    ux, uy = get_user_position()
+    ax, ay = get_anchor_position(anchor_id)
+    distance = ((ax-ux)**2+(ay-uy)**2)**0.5
+    return distance
 
 def addOrUpdateAnchorMapping(anchor, sequence):
     jsonFile = open("store.json", "r")
@@ -58,6 +70,22 @@ def addOrUpdateAnchorMapping(anchor, sequence):
     jsonFile = open("store.json", "w")
     jsonFile.write(json.dumps(mappings))
     jsonFile.close()
+
+# We should play the vibrations on the motor facing the direction of the anchor
+# To do this we need to get the direction of the anchor
+# Method exposed by haptic output should take direction as an argument
+def indicate_distance(anchor_id):
+    print("Indicating Distance")
+    distance = get_distance_from_anchor(anchor_id)
+    print("Distance from anchor:", distance)
+    if distance > 10:
+        flash_leds(delay=2)
+    elif distance > 7:
+        flash_leds(delay=1)
+    elif distance > 4:
+        flash_leds(delay=0.5, no_of_flashes=6)
+    else:
+        flash_leds(delay=0.25, no_of_flashes=6)
 
 def loop_sequence(button_states):
     if (any(button_states)):
@@ -78,7 +106,7 @@ def loop_sequence(button_states):
             time.sleep(0.7)
             print_sequence(sequence_to_play)
             play_sequence(sequence_to_play)
-    
+            indicate_distance(closest_anchor)
 
 def record_sequence(button_states, counter):
     print("switch on", button_states, switch_on)
@@ -101,7 +129,7 @@ def record_sequence(button_states, counter):
         closest_anchor = get_closest_anchor()
         addOrUpdateAnchorMapping(closest_anchor, sequence)
         time.sleep(0.5)
-        flash_leds()
+        flash_leds(delay=0.2)
         return 0
 
     return counter
