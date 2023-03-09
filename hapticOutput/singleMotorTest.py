ae936ebc82eb06829724cec7e74cb3c4b3e2fd5e
asfhja <easfa>
2023-03-09 03:17:43 +0000

    Add script to test single motor

---
+++ hapticOutput/singleMotorTest.py
@@ -0,0 +1,17 @@
+import board, time
+import adafruit_tca9548a
+import adafruit_drv2605
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+time.sleep(1)
+
+# Create the TCA9548A object and give it the I2C bus
+i2cExpander = adafruit_tca9548a.TCA9548A(i2c)
+
+# Create DRV2605 object and give it an SDA+SCL on the i2cExpander
+drv = adafruit_drv2605.DRV2605(i2cExpander[1])
+
+while True:
+    drv.sequence[0] = adafruit_drv2605.Effect(51)
+    drv.play()
+    time.sleep(2)
