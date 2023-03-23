c612728747c9f9ddec1e5d1ede5d7ca73d5b622f
Tom Green <tom.31.green@gmail.com>
2023-03-23 11:35:28 +0000

    Tidy up the code

--- astar_search.py
+++ astar_search.py
@@ -38,33 +38,26 @@ def return_path(current_node):
     while current is not None:
         path.append(current.position)
         current = current.parent
-    return path[::-1]  # Return reversed path
+    return path[::-1]
 
 
-def Origin_Offset():
+def origin_offset():
     # Find the minimum x and y coord values from anchors JSON
-    # HardCode for now
-    Min_x = -2.3
-    Min_y = -0.8
-    return (Min_x, Min_y)
+    # hard code it for now
+    min_x = -2.3
+    min_y = -0.8
+    return (min_x, min_y)
 
 
-def get_currentlocation(maze, node_density):
-    # TODO: will need to pull from Rego's data
-    rand_start = (randint(0, (len(maze) - 1))*2, randint(0, (len(maze) - 1))*2)
-    start = (rand_start[0]//node_density, rand_start[1]//node_density)
-    return start
-
-
-def get_goal(maze, node_density):
+def get_goal(map, node_density):
     # TODO: will need to pull from Kaif's data
     incomplete = True
     while incomplete:
-        rand_end = (randint(0, (len(maze) - 1))*2,
-                    randint(0, (len(maze) - 1))*2)
-        end = (rand_end[0]//node_density, rand_end[1]//node_density)
+        rand_end = (int(randint(0, (len(map) - 1))*2),
+                    int(randint(0, (len(map) - 1))*2))
+        end = (int(rand_end[0]//node_density), int(rand_end[1]//node_density))
 
-        if maze[end[0]][end[1]] != 0:
+        if map[end[0]][end[1]] != 0:
             continue
         else:
             incomplete = False
@@ -72,12 +65,7 @@ def get_goal(maze, node_density):
     return end
 
 
-def get_current_heading():
-    heading = randint(0, 360)
-    return heading
-
-
-def get_heading_needed(path):
+def get_heading_required(path):
     if len(path) == 1:
         target_heading = 0
     else:
@@ -105,28 +93,66 @@ def get_heading_needed(path):
     return target_heading
 
 
-def Regos_function(maze):
+# TODO actually pull from regos data, at which point won't need map input
+def get_location_and_heading(map):
 
-    Location_x = randint(0, (len(maze)-1)*10/2)/10
-    Location_y = randint(0, (len(maze[0])-1)*10/2)/10
-    Heading = randint(0, 360)
+    x_location = randint(0, (len(map)-1)*10/2)/10
+    y_location = randint(0, (len(map[0])-1)*10/2)/10
+    heading = randint(0, 360)
 
-    return (Location_x, Location_y, Heading)
-
-
-# def convert_to_map(path, x_max, y_max):
-#     iteration = 0
-#     for step in path:
-#         path[iteration][0] = path[iteration][0]
-#         path[iteration][1] =
-
-#         iteraion += 1
+    return (x_location, y_location, heading)
 
 
-def astar(maze, start, end, allow_diagonal_movement=True):
+def print_map_fun(map, path, start, end):
+    for step in path:
+        map[step[0]][step[1]] = 2
+
+        map[start[0]][start[1]] = 3
+        map[end[0]][end[1]] = 4
+
+    for col in map:
+        line = []
+        for row in col:
+            if row == 1:
+                line.append("\u2588"*3)
+            elif row == 0:
+                line.append(" . ")
+            elif row == 2:
+                line.append(" # ")
+            elif row == 3:
+                line.append(" O ")
+            elif row == 4:
+                line.append(" x ")
+        print("".join(line))
+
+
+def map_angle_to_direction(heading_change):
+    heading_change = round(8*(heading_change)/360, 0)
+
+    if heading_change == 0 or heading_change == 8 or heading_change == -8:
+        turn_direction = 'N'
+    elif heading_change == 1 or heading_change == -7:
+        turn_direction = 'NE'
+    elif heading_change == 2 or heading_change == -6:
+        turn_direction = 'E'
+    elif heading_change == 3 or heading_change == -5:
+        turn_direction = 'SE'
+    elif heading_change == 4 or heading_change == -4:
+        turn_direction = 'S'
+    elif heading_change == 5 or heading_change == -3:
+        turn_direction = 'SW'
+    elif heading_change == 6 or heading_change == -2:
+        turn_direction = 'W'
+    elif heading_change == 7 or heading_change == -1:
+        turn_direction = 'NW'
+
+    return (turn_direction)
+
+
+def astar(map, start, end, allow_diagonal_movement=True):
     """
-    Returns a list of tuples as a path from the given start to the given end in the given maze
-    :param maze:
+    Returns a list of tuples as a path from the given start to the given end in the given map
+    :param map:
     :param start:
     :param end:
     :return:
@@ -142,13 +168,13 @@ def astar(maze, start, end, allow_diagonal_movement=True):
     open_list = []
     closed_list = []
 
-    # Heapify the open_list and Add the start node
+    # heapify the open_list and Add the start node
     heapq.heapify(open_list)
     heapq.heappush(open_list, start_node)
 
     # Adding a stop condition
     outer_iterations = 0
-    max_iterations = 512  # (len(maze[0]) * len(maze) // 2)
+    max_iterations = 512  # (len(map[0]) * len(map) // 2)
 
     # what squares do we search
     adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
@@ -184,11 +210,11 @@ def astar(maze, start, end, allow_diagonal_movement=True):
                 current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
             # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[0]) - 1) or node_position[1] < 0:
+            if node_position[0] > (len(map) - 1) or node_position[0] < 0 or node_position[1] > (len(map[0]) - 1) or node_position[1] < 0:
                 continue
 
             # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] != 0:
+            if map[node_position[0]][node_position[1]] != 0:
                 continue
 
             # Check diagonal movement is valid (i.e. they don't need to walk through a wall)
@@ -199,7 +225,7 @@ def astar(maze, start, end, allow_diagonal_movement=True):
                 right_node_y = node_position[1] - new_position[1]
 
                 # Check validity of nodes next to diagnonal
-                if maze[left_node_x][left_node_y] != 0 or maze[right_node_x][right_node_y] != 0:
+                if map[left_node_x][left_node_y] != 0 or map[right_node_x][right_node_y] != 0:
                     continue
 
             # Create new node
@@ -210,19 +236,21 @@ def astar(maze, start, end, allow_diagonal_movement=True):
 
         # Loop through children
         for child in children:
+
             # Child is on the closed list
             if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
                 continue
 
-            # Create the f, g, and h values
+            # Create the g, h and f values
+            # TODO the issue coudld be there's a much greater bias towards h than g
+            child.h = (((child.position[0] - end_node.position[0]) ** 2) + (
+                (child.position[1] - end_node.position[1]) ** 2)) ** 0.5
+
             if abs(child.position[0]) == abs(child.position[1]):  # Diagonal move
                 child.g = current_node.g + 1.414
             else:
                 child.g = current_node.g + 1
-            # child.g = current_node.g + 1 #TODO change to if statement for diagonals to be sqrt(2)
 
-            child.h = (((child.position[0] - end_node.position[0]) **
-                       2) + ((child.position[1] - end_node.position[1]) ** 2)) ** 0.5  # TODO the issue is there's a much greater bias towards h than g
             child.f = child.g + child.h
 
             # Child is already in the open list
@@ -236,97 +264,45 @@ def astar(maze, start, end, allow_diagonal_movement=True):
     return None
 
 
-def example(print_maze=False, print_path=False):
+def turn_required(print_map=False, print_path=False):
     destination_reached = False
-    maze = [[0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-            [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
-            [0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],]
+    map = [[0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+           [0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+           [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+           [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+           [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+           [0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+           [0, 1, 0, 1, 0, 1, 0, 1, 0, 0],
+           [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],]
 
-    node_density = 2
+    # Define node density
+    distance_between_nodes = 0.5
+    node_density = 1/distance_between_nodes
 
-    # Start and end override for testing
-    # start = (6, 0)
-    # end = (1, 9)
+    # Get x,y,h from localisation subsystem
+    [x, y, h] = get_location_and_heading(map)  # TODO remove map input
 
-    # Get X,Y,H from Rego's function
-    # maze input only necessary for the rand gen
-    [X, Y, H] = Regos_function(maze)
-
-    start = (int(round(X*node_density, 0)), int(round(Y*node_density, 0)))
-
-    end = get_goal(maze, node_density)
-
-    # print("Start location is: {}".format(start))
-    # print("End location is: {}".format(end))
+    # Define start and end coords
+    start = (int(round(x*node_density, 0)), int(round(y*node_density, 0)))
+    end = get_goal(map, node_density)
 
     if start == end:
         destination_reached = True
 
-    path = astar(maze, start, end)
+    path = astar(map, start, end)
 
-    if print_maze:
-        for step in path:
-            maze[step[0]][step[1]] = 2
-
-        maze[start[0]][start[1]] = 3
-        maze[end[0]][end[1]] = 4
-
-        for col in maze:
-            line = []
-            for row in col:
-                if row == 1:
-                    line.append("\u2588"*3)
-                elif row == 0:
-                    line.append(" . ")
-                elif row == 2:
-                    line.append(" # ")
-                elif row == 3:
-                    line.append(" O ")
-                elif row == 4:
-                    line.append(" X ")
-            print("".join(line))
-
-    # path = convert_to_map(path, len(maze[0]), len(maze))
+    if print_map:
+        print_map_fun(map, path, start, end)
 
     if print_path:
         print(path)
 
-    # Heading calculations - Dont Delete
+    # heading calculations
+    required_heading = get_heading_required(path)
+    heading_change = required_heading - h
 
-    required_heading = get_heading_needed(path)
-    heading_change = round(8*(required_heading - H)/360, 0)
-    # current_heading = round(8*get_heading()/360, 0)
-
-    if heading_change == 0 or heading_change == 8 or heading_change == -8:
-        turn_direction = 'N'
-    elif heading_change == 1 or heading_change == -7:
-        turn_direction = 'NE'
-    elif heading_change == 2 or heading_change == -6:
-        turn_direction = 'E'
-    elif heading_change == 3 or heading_change == -5:
-        turn_direction = 'SE'
-    elif heading_change == 4 or heading_change == -4:
-        turn_direction = 'S'
-    elif heading_change == 5 or heading_change == -3:
-        turn_direction = 'SW'
-    elif heading_change == 6 or heading_change == -2:
-        turn_direction = 'W'
-    elif heading_change == 7 or heading_change == -1:
-        turn_direction = 'NW'
-
-    # print("current heading is: {} ".format(H))
-    # print("desired heading is: {} ".format(required_heading))
-    # print("heading change required is: {} ".format(heading_change))
+    turn_direction = map_angle_to_direction(heading_change)
 
     return (turn_direction, destination_reached)
-
-
-Output = example()
-print(Output)
