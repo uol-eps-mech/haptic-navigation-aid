2bcead12a32a289367e143a30aa2f226cb0adc56
Tom Green <tom.31.green@gmail.com>
2023-03-23 09:53:45 +0000

    Update output to be (turnDirection,Arrived_flag)

--- astar_search.py
+++ astar_search.py
@@ -41,6 +41,14 @@ def return_path(current_node):
     return path[::-1]  # Return reversed path
 
 
+def Origin_Offset():
+    # Find the minimum x and y coord values from anchors JSON
+    # HardCode for now
+    Min_x = -2.3
+    Min_y = -0.8
+    return (Min_x, Min_y)
+
+
 def get_currentlocation(maze, node_density):
     # TODO: will need to pull from Rego's data
     rand_start = (randint(0, (len(maze) - 1))*2, randint(0, (len(maze) - 1))*2)
@@ -64,34 +72,55 @@ def get_goal(maze, node_density):
     return end
 
 
-def get_heading():
+def get_current_heading():
     heading = randint(0, 360)
     return heading
 
 
-def get_heading_needed(route):
-    required_movement_direction_x = route[1][0] - route[0][0]
-    required_movement_direction_y = route[1][1] - route[0][1]
-    required_movement_direction = (
-        required_movement_direction_x, required_movement_direction_y)
-
-    if required_movement_direction == (0, 1):
+def get_heading_needed(path):
+    if len(path) == 1:
         target_heading = 0
-    elif required_movement_direction == (1, 1):
-        target_heading = 45
-    elif required_movement_direction == (1, 0):
-        target_heading = 90
-    elif required_movement_direction == (1, -1):
-        target_heading = 135
-    elif required_movement_direction == (0, -1):
-        target_heading = 180
-    elif required_movement_direction == (-1, -1):
-        target_heading = 225
-    elif required_movement_direction == (-1, 0):
-        target_heading = 270
-    elif required_movement_direction == (-1, 1):
-        target_heading = 315
+    else:
+        required_movement_direction_x = path[1][0] - path[0][0]
+        required_movement_direction_y = path[1][1] - path[0][1]
+        required_movement_direction = (
+            required_movement_direction_x, required_movement_direction_y)
+
+        if required_movement_direction == (0, 1):
+            target_heading = 0
+        elif required_movement_direction == (1, 1):
+            target_heading = 45
+        elif required_movement_direction == (1, 0):
+            target_heading = 90
+        elif required_movement_direction == (1, -1):
+            target_heading = 135
+        elif required_movement_direction == (0, -1):
+            target_heading = 180
+        elif required_movement_direction == (-1, -1):
+            target_heading = 225
+        elif required_movement_direction == (-1, 0):
+            target_heading = 270
+        elif required_movement_direction == (-1, 1):
+            target_heading = 315
     return target_heading
+
+
+def Regos_function(maze):
+
+    Location_x = randint(0, (len(maze)-1)*10/2)/10
+    Location_y = randint(0, (len(maze[0])-1)*10/2)/10
+    Heading = randint(0, 360)
+
+    return (Location_x, Location_y, Heading)
+
+
+# def convert_to_map(path, x_max, y_max):
+#     iteration = 0
+#     for step in path:
+#         path[iteration][0] = path[iteration][0]
+#         path[iteration][1] =
+
+#         iteraion += 1
 
 
 def astar(maze, start, end, allow_diagonal_movement=True):
@@ -155,7 +184,7 @@ def astar(maze, start, end, allow_diagonal_movement=True):
                 current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
             # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) - 1) or node_position[1] < 0:
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[0]) - 1) or node_position[1] < 0:
                 continue
 
             # Make sure walkable terrain
@@ -207,8 +236,8 @@ def astar(maze, start, end, allow_diagonal_movement=True):
     return None
 
 
-def example(print_maze=True):
-
+def example(print_maze=False, print_path=False):
+    destination_reached = False
     maze = [[0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
@@ -221,12 +250,24 @@ def example(print_maze=True):
             [0, 0, 0, 1, 0, 0, 0, 1, 0, 0],]
 
     node_density = 2
-    # start = get_currentlocation(maze)
-    # end = get_goal(maze)
-    # start = get_currentlocation(maze, node_density)
-    # end = get_goal(maze, node_density)
-    start = (0, 0)
-    end = (9, 9)
+
+    # Start and end override for testing
+    # start = (6, 0)
+    # end = (1, 9)
+
+    # Get X,Y,H from Rego's function
+    # maze input only necessary for the rand gen
+    [X, Y, H] = Regos_function(maze)
+
+    start = (int(round(X*node_density, 0)), int(round(Y*node_density, 0)))
+
+    end = get_goal(maze, node_density)
+
+    # print("Start location is: {}".format(start))
+    # print("End location is: {}".format(end))
+
+    if start == end:
+        destination_reached = True
 
     path = astar(maze, start, end)
 
@@ -237,37 +278,55 @@ def example(print_maze=True):
         maze[start[0]][start[1]] = 3
         maze[end[0]][end[1]] = 4
 
-        for row in maze:
+        for col in maze:
             line = []
-            for col in row:
-                if col == 1:
+            for row in col:
+                if row == 1:
                     line.append("\u2588"*3)
-                elif col == 0:
+                elif row == 0:
                     line.append(" . ")
-                elif col == 2:
+                elif row == 2:
                     line.append(" # ")
-                elif col == 3:
+                elif row == 3:
                     line.append(" O ")
-                elif col == 4:
+                elif row == 4:
                     line.append(" X ")
             print("".join(line))
 
-    return (path)
+    # path = convert_to_map(path, len(maze[0]), len(maze))
+
+    if print_path:
+        print(path)
+
+    # Heading calculations - Dont Delete
+
+    required_heading = get_heading_needed(path)
+    heading_change = round(8*(required_heading - H)/360, 0)
+    # current_heading = round(8*get_heading()/360, 0)
+
+    if heading_change == 0 or heading_change == 8 or heading_change == -8:
+        turn_direction = 'N'
+    elif heading_change == 1 or heading_change == -7:
+        turn_direction = 'NE'
+    elif heading_change == 2 or heading_change == -6:
+        turn_direction = 'E'
+    elif heading_change == 3 or heading_change == -5:
+        turn_direction = 'SE'
+    elif heading_change == 4 or heading_change == -4:
+        turn_direction = 'S'
+    elif heading_change == 5 or heading_change == -3:
+        turn_direction = 'SW'
+    elif heading_change == 6 or heading_change == -2:
+        turn_direction = 'W'
+    elif heading_change == 7 or heading_change == -1:
+        turn_direction = 'NW'
+
+    # print("current heading is: {} ".format(H))
+    # print("desired heading is: {} ".format(required_heading))
+    # print("heading change required is: {} ".format(heading_change))
+
+    return (turn_direction, destination_reached)
 
 
-route = example()
-print(route)
-# heading_change = get_heading_needed(route) - get_heading()
-
-
-current_heading = get_heading()
-# current_heading = round(8*get_heading()/360, 0)
-print("current heading is: {} ".format(current_heading))
-
-
-required_heading = get_heading_needed(route)
-print("desired heading is: {} ".format(required_heading))
-
-heading_change = round(required_heading - current_heading)
-# heading_change = round(8*(required_heading - current_heading)/360, 0)
-print("heading change is: {} ".format(heading_change))
+Output = example()
+print(Output)
