856665e8ebcec0ab3942bf91cfd20817b838c204
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-03-28 11:10:45 +0100

    Use command line argument for single motor test

--- hapticOutput/singleMotorTest.py
+++ hapticOutput/singleMotorTest.py
@@ -1,6 +1,7 @@
 import board, time
 import adafruit_tca9548a
 import adafruit_drv2605
+import sys
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 time.sleep(1)
@@ -9,7 +10,7 @@ time.sleep(1)
 i2cExpander = adafruit_tca9548a.TCA9548A(i2c)
 
 # Create DRV2605 object and give it an SDA+SCL on the i2cExpander
-drv = adafruit_drv2605.DRV2605(i2cExpander[1])
+drv = adafruit_drv2605.DRV2605(i2cExpander[int(sys.argv[1])])
 
 while True:
     drv.sequence[0] = adafruit_drv2605.Effect(51)
