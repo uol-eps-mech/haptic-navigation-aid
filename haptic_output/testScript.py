b42f291c0ff92d7df5a16d87c8a2988451e75906
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-04-01 17:18:12 +0100

    MOdify test script to play direction thrice

--- haptic_output/testScript.py
+++ haptic_output/testScript.py
@@ -1,60 +1,69 @@
-from hapticOutput import play_direction
+import haptic_output
 import keyboard
 from time import sleep
+import adafruit_tca9548a
+import board
 
 high_intensity_effect_id = 17
 low_intensity_effect_id = 20
 effectsl=[low_intensity_effect_id, 19, high_intensity_effect_id]
 
+# uses board.SCL and board.SDA
+i2c = board.I2C()  
+# Create the TCA9548A object and give it the I2C bus
+i2cExpander = adafruit_tca9548a.TCA9548A(i2c)
+haptic_output = haptic_output.HapticOutput(i2cExpander)
+direction = ""
+
 while True:
     try:
         if keyboard.is_pressed('q'):
-            play_direction("N", 3, effects=effectsl)
+            haptic_output.play_direction("N", intensity=3, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('w'):
-            play_direction("S", 3, effects=effectsl)
+            haptic_output.play_direction("NE", intensity=3, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('e'):
-            play_direction("E", 3, effects=effectsl)
+            haptic_output.play_direction("E", intensity=3, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('r'):
-            play_direction("W", 3, effects=effectsl)
+            haptic_output.play_direction("SE", intensity=3, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('t'):
-            play_direction("NE", 3, effects=effectsl)
+            haptic_output.play_direction("S", intensity=3, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('y'):
-            play_direction("SE", 3, effects=effectsl)
+            haptic_output.play_direction("SW", intensity=3, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('u'):
-            play_direction("NW", 3, effects=effectsl)
+            haptic_output.play_direction("W", intensity=3, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('i'):
-            play_direction("SW", 3, effects=effectsl)
+            haptic_output.play_direction("NW", intensity=3, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('a'):
-            play_direction("N", 1, effects=effectsl)
+            haptic_output.play_direction("N", intensity=1, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('s'):
-            play_direction("S", 1, effects=effectsl)
+            haptic_output.play_direction("NE", intensity=1, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('d'):
-            play_direction("E", 1, effects=effectsl)
+            haptic_output.play_direction("E", intensity=1, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('f'):
-            play_direction("W", 1, effects=effectsl)
+            haptic_output.play_direction("SE", intensity=1, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('g'):
-            play_direction("NE", 1, effects=effectsl)
+            haptic_output.play_direction("S", intensity=1, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('h'):
-            play_direction("SE", 1, effects=effectsl)
+            haptic_output.play_direction("SW", intensity=1, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('j'):
-            play_direction("NW", 1, effects=effectsl)
+            haptic_output.play_direction("W", intensity=1, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('k'):
-            play_direction("SW", 1, effects=effectsl)
+            haptic_output.play_direction("NW", intensity=1, count=3, delay=0.5, effects=effectsl)
             sleep(0.5)
         if keyboard.is_pressed('1'):
             break
