23f851841fe52d67c1aad2428886784cbda686ca
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-04-18 13:34:25 +0100

    Handle out of range errors

--- path_planning/path_planning.py
+++ path_planning/path_planning.py
@@ -279,8 +279,11 @@ def astar(map, start, end, allow_diagonal_movement=True):
                 child.g = current_node.g + 1
 
             for cell in get_one_cell_radius(child.position[0], child.position[1]):
+                try:
                 if map[cell[0]][cell[1]] == 0:
                     child.g += 0.5
+                except:
+                    pass
 
             child.f = child.g + child.h
 
