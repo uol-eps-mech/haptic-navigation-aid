91e9a30b328a661eb73d8bd19b08aca7a9a31e8a
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-04-21 15:37:07 +0100

    Use machine library and play haptic motor when switch state changes

--- haptic_input/haptic_input_v2.py
+++ haptic_input/haptic_input_v2.py
@@ -1,8 +1,7 @@
 import time
-import random
 import network
-import json
-from picozero import RGBLED, Button, Switch, pico_led
+from machine import Pin
+from picozero import pico_led
 import urequests
 
 debounce = 0.3  # seconds
@@ -14,11 +13,15 @@ action_button = 5
 buttonPins = [0, 1, 2, 3, 4, 5]
 buttons = []
 
-colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255),
-          (255, 255, 0), (0, 255, 255), (255, 0, 255)]
+colors = [(1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0)]
 
-led = RGBLED(13, 14, 15)
-switch = Switch(16)
+R = Pin(13, Pin.OUT, Pin.PULL_DOWN)
+G = Pin(14, Pin.OUT, Pin.PULL_DOWN)
+B = Pin(15, Pin.OUT, Pin.PULL_DOWN)
+led = [R,G,B]
+
+switch = Pin(16, Pin.IN)
+motor = Pin(6, Pin.OUT, Pin.PULL_DOWN)
 
 ssid = 'Johnny'
 password = '12345678990'
@@ -46,44 +49,53 @@ def connect():
         pico_led.on()
 
 
-def turn_off_leds():
-    led.off()
-
+def turn_off_led():
+    for pin in led:
+        led[pin].value(0)
 
 def turn_on_leds():
-    led.on()
+    for pin in led:
+        led[pin].value(1)
 
+def set_led_color(color):
+    for id, val in enumerate(color):
+        led[id].value(val)
+
+def play_haptic_motor():
+    motor.value(1)
+    time.sleep(0.5)
+    motor.value(0)
 
 def setup():
     for pin in buttonPins:
-        btn = Button(pin, pull_up=False, bounce_time=debounce)
+        btn = Pin(pin, Pin.IN, Pin.PULL_DOWN)
         buttons.append(btn)
     connect()
     sequence.clear()
     destination_sequence.clear()
-    turn_off_leds()
+    turn_off_led()
 
 
 def get_input_states():
     buttonStates = get_buttons_state()
-    switchState = switch.value
+    switchState = switch.value()
     return buttonStates, switchState
 
 
 def get_buttons_state():
     button_states = []
     for button in buttons:
-        button_states.append(button.value)
+        button_states.append(button.value())
     return button_states
 
 
 def flash_leds():
     for _ in range(1):
-        turn_off_leds()
+        turn_off_led()
         time.sleep(0.2)
         turn_on_leds()
         time.sleep(0.2)
-        turn_off_leds()
+        turn_off_led()
         time.sleep(0.5)
 
 
@@ -93,21 +105,14 @@ def print_sequence(sequence):
     print("-----------------------------------------------")
 
 
-def update_destination(sequence):
+def send_update_destination_request(sequence):
     print("sending map sequence request")
     request_url = base_url + "/updatedestination/" + str(sequence)
     response = urequests.get(str(request_url))
     response.close()
 
 
-def get_location():
-    print("sending get location request")
-    request_url = base_url + "/getlocation"
-    response = urequests.get(str(request_url))
-    response.close()
-
-
-def map_sequence(sequence):
+def send_map_sequence_request(sequence):
     if (not sequence or len(sequence) <= 1 or not all(int(id) <= 5 for id in sequence)):
         print("invalid sequence")
         return
@@ -117,21 +122,21 @@ def map_sequence(sequence):
     response.close()
 
 
-def play_ack_sequence():
+def send_play_ack_sequence_request():
     print("Sending ack_sequence request")
     request_url = base_url + "/playacksequence"
     response = urequests.get(str(request_url))
     response.close()
 
 
-def play_sequence(sequence):
+def send_play_sequence_request(sequence):
     print("sending play sequence request")
     request_url = base_url + "/playsequence/" + str(",".join(sequence))
     response = urequests.get(str(request_url))
     response.close()
 
 
-def set_destination(sequence):
+def send_set_destination_request(sequence):
     if (not sequence or len(sequence) <= 1 or not all(int(id) <= 5 for id in sequence)):
         print("invalid sequence")
         return
@@ -141,12 +146,12 @@ def set_destination(sequence):
     response.close()
 
 
-def play_button(button):
-    if (not (int(button) <= 5 and int(button) >= 0)):
-        print("invalid button")
+def send_play_motor_request(motor):
+    if (not (int(motor) <= 5 and int(motor) >= 0)):
+        print("invalid motor")
         return
-    print("sending play button request")
-    request_url = base_url + "/playbutton/" + str(button)
+    print("sending play motor request")
+    request_url = base_url + "/playmotor/" + str(motor)
     response = urequests.get(str(request_url))
     response.close()
 
@@ -159,18 +164,18 @@ def record_sequence(buttonStates):
                     led.on()
                     led.color = colors[id]
                     sequence.append(str(id))
-                    play_button(id)
+                    send_play_motor_request(id)
                     time.sleep(debounce)
-                    turn_off_leds()
+                    turn_off_led()
                     print("Button", id+1, "pressed")
                     print("Current Sequence:", sequence)
             return True
         else:
-            play_button(action_button)
+            send_play_motor_request(action_button)
             time.sleep(0.5)
-            map_sequence(sequence)
+            send_map_sequence_request(sequence)
             flash_leds()
-            play_sequence(sequence)
+            send_play_sequence_request(sequence)
             sequence.clear()
             return False
     return True
@@ -181,19 +186,18 @@ def get_destination_sequence(buttonStates):
         if (not buttonStates[action_button]):
             for id, state in enumerate(buttonStates):
                 if state == 1:
-                    led.on()
-                    led.color = colors[id]
+                    set_led_color(colors[id])
                     destination_sequence.append(str(id))
-                    play_button(id)
+                    send_play_motor_request(id)
                     time.sleep(debounce)
-                    turn_off_leds()
+                    turn_off_led()
                     print("Button", id+1, "pressed")
                     print("Current Sequence:", destination_sequence)
             return True
         else:
-            play_button(action_button)
+            send_play_motor_request(action_button)
             time.sleep(0.5)
-            set_destination(destination_sequence)
+            send_set_destination_request(destination_sequence)
             flash_leds()
             destination_sequence.clear()
             return False
@@ -208,29 +212,38 @@ while True:
     print("here")
     recording_mapping_sequence = False
     recording_destination_sequence = False
+    previous_state = switchOn
 
     while (switchOn):
         buttonStates, switchOn = get_input_states()
         if (not switchOn):
             break
 
+        if (switchOn != previous_state):
+            play_haptic_motor()
+
         if (not any(buttonStates)):
             continue
 
         if (buttonStates[action_button] and not recording_mapping_sequence):
             print("recording mapping sequence")
-            play_button(action_button)
+            send_play_motor_request(action_button)
             recording_mapping_sequence = True
             time.sleep(debounce)
             continue
 
         if (recording_mapping_sequence):
             recording_mapping_sequence = record_sequence(buttonStates)
+        
+        previous_state = switchOn
 
     while (not switchOn):
         buttonStates, switchOn = get_input_states()
         if (switchOn):
             break
+
+        if (switchOn != previous_state):
+            play_haptic_motor()
 
         if (not any(buttonStates)):
             continue
@@ -239,7 +252,7 @@ while True:
 
         if (buttonStates[action_button] and not recording_destination_sequence):
             print("recording destination sequence")
-            play_button(action_button)
+            send_play_motor_request(action_button)
             recording_destination_sequence = True
             time.sleep(1)
             continue
@@ -247,3 +260,5 @@ while True:
         if (recording_destination_sequence):
             recording_destination_sequence = get_destination_sequence(
                 buttonStates)
+            
+        previous_state = switchOn
