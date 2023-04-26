13efa1720323c23c9184608d39d90ce524064630
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-04-26 13:16:12 +0100

    Debug

--- localisation/test_bno055.py
+++ localisation/test_bno055.py
@@ -52,8 +52,10 @@ class Localisation:
 
 def test_bno055():
     i2c = board.I2C()
-    i2cExpander = adafruit_tca9548a.TCA9548A(i2c)
-    localisation = Localisation(i2cExpander[6])
+    # i2cExpander = adafruit_tca9548a.TCA9548A(i2c)
+    # localisation = Localisation(i2cExpander[6])
+    localisation = Localisation(i2c)
+
     while True:
         heading = localisation.get_user_heading()
         print(heading)
