5334b888304ab230de66426b8a91239ff69dd944
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-06-22 11:24:39 +0100

    Indicate destination and direction to user for nearest landmark

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI
 from haptic_output.haptic_output import HapticOutput
 from localisation.localisation import Localisation
-from path_planning.path_planning import PathPlanner
+from path_planning.path_planning import PathPlanner, Node
 import json
 import board
 import adafruit_tca9548a
 import math
-import random
-import time
-from free_points import free_points
 
 acknowledgement_effect_id = 47
 error_effect_id = 27
@@ -104,33 +101,36 @@ def get_destination():
 
 
 def find_nearest_landmark(x, y):
+    # Get landmark mappings
     jsonFile = open("store.json", "r")
     mappings = json.load(jsonFile)["mappings"]
     jsonFile.close()
 
     try:
+        # Search for nearest landmark
         shortest_distance = 200
-        nearest_landmark = None
+        nearest_landmark, sequence, direction = None
         for location in mappings:
             distance = math.dist((x, y), eval(location))
             if (distance < shortest_distance):
-                nearest_landmark = (eval(location), mappings[location])
+                nearest_landmark = eval(location)
+                sequence = mappings[location]
                 shortest_distance = distance
-        return (None, None) if nearest_landmark == None else nearest_landmark
+
+        # If not found return None
+        if nearest_landmark == None:
+            return (nearest_landmark, sequence, direction, None)
+
+        # Determine direction to nearest landmark
+        heading = path_planner.__get_target_heading(
+            Node(None, (x, y)), Node(None, nearest_landmark))
+        direction = path_planner.__map_angle_to_direction(heading)
+
+        # Return data
+        return (nearest_landmark, sequence, direction, shortest_distance)
+
     except:
-        return (None, None)
-
-
-def add_execution_time(time, start, end):
-    file = open("update_execution_times.csv", "a+")
-    file.write(str(time) + "," + str(start) + "," + str(end) + "\n")
-    file.close()
-
-
-def add_to_user_path(position):
-    file = open("user_paths.csv", "a+")
-    file.write(str(position) + "\n")
-    file.close()
+        return (None, None, None, None)
 
 
 def round(x):
@@ -141,6 +141,10 @@ def round(x):
         return base
 
 
+def error():
+    haptic_output.play_effect(error_effect_id, 0.5, 4)
+
+
 @app.get("/")
 async def root():
     print("Hello World")
@@ -149,167 +153,160 @@ async def root():
 
 @app.get("/playobstacle/{direction}")
 def play_obstacle(direction):
-    print("Play Obstacle Request Received")
-    haptic_output.inidicate_obstacle(format_sequence_bool(direction))
-    return {"message": "Played direction: " + str(direction)}
+    try:
+        print("Play Obstacle Request Received")
+        haptic_output.inidicate_obstacle(format_sequence_bool(direction))
+        return {"message": "Played direction: " + str(direction)}
+    except Exception as e:
+        error()
+        return {"message": "Error Occured" + str(e)}
 
 
 @app.get("/mapsequence/{sequence}")
 def map_sequence(sequence):
-    print("Map Sequence Request received")
-    x, y = localisation.get_user_position()
-    add_or_update_sequence_mapping(
-        str((x, y)), format_sequence_int(sequence))
-    return {"message": "Sequence '" + sequence + "' mapped to location: '" + str((x, y)) + "'"}
+    try:
+        print("Map Sequence Request received")
+        x, y = localisation.get_user_position()
+        add_or_update_sequence_mapping(
+            str((x, y)), format_sequence_int(sequence))
+        return {"message": "Sequence '" + sequence + "' mapped to location: '" + str((x, y)) + "'"}
+    except Exception as e:
+        error()
+        return {"message": "Error Occured" + str(e)}
 
 
 @app.get("/playacksequence")
 def play_ack_sequence():
-    print("PLay ack Request received")
-    haptic_output.play_effect(acknowledgement_effect_id, 0.5, 2)
-    return {"message": "playing acknowledgement sequence"}
+    try:
+        print("Play ack Request received")
+        haptic_output.play_effect(acknowledgement_effect_id, 0.5, 2)
+        return {"message": "playing acknowledgement sequence"}
+    except Exception as e:
+        error()
+        return {"message": "Error Occured" + str(e)}
 
 
 @app.get("/playsequence/{sequence}")
 def play_entered_sequence(sequence):
-    print("PLay sequence Request received")
-    if isinstance(sequence, str) or isinstance(sequence[0], str):
-        sequence = format_sequence_int(sequence)
+    try:
+        print("Play sequence request received")
+        if isinstance(sequence, str) or isinstance(sequence[0], str):
+            sequence = format_sequence_int(sequence)
 
-    print("playing sequence", sequence)
-    haptic_output.play_sequence(sequence)
-    return {"message": "playing sequence: " + str(sequence)}
+        print("Playing sequence", sequence)
+        haptic_output.play_sequence(sequence)
+        return {"message": "Playing sequence: " + str(sequence)}
+
+    except Exception as e:
+        error()
+        return {"message": "Error Occured" + str(e)}
 
 
 def play_location_sequence(location):
-    print("PLaying location sequence")
-    seqeunce = get_sequence_for_location(location)
-    play_entered_sequence(seqeunce)
+    try:
+        print("PLaying location sequence")
+        seqeunce = get_sequence_for_location(location)
+        play_entered_sequence(seqeunce)
+    except Exception as e:
+        error()
+        return {"message": "Error Occured" + str(e)}
 
 
 @app.get("/playmotor/{motor}")
 def play_button(motor):
-    print("Playing motor", motor)
-    haptic_output.play_motor(int(motor))
-    return {"message": "playing motor: '" + motor + "'"}
+    try:
+        print("Playing motor", motor)
+        haptic_output.play_motor(int(motor))
+        return {"message": "playing motor: '" + motor + "'"}
+    except Exception as e:
+        error()
+        return {"message": "Error Occured" + str(e)}
 
 
 @app.get("/getlocation/{sequence}")
 def get_location(sequence):
-    location = get_location_from_sequence(format_sequence_int(sequence))
-    return {"message": location}
+    try:
+        location = get_location_from_sequence(format_sequence_int(sequence))
+        return {"message": location}
+    except Exception as e:
+        error()
+        return {"message": "Error Occured" + str(e)}
 
 
 @app.get("/getnearestlandmark")
 def get_nearest_landmark():
-    destination = get_destination()
-    update_destination_location(None)
-    x, y = localisation.get_user_position()
-    landmark, sequence = find_nearest_landmark(x, y)
-    if (landmark == None):
-        play_ack_sequence()
-        return {"message": "Nearest landmark found not found."}
-    else:
-        play_entered_sequence(sequence)
+    try:
+        # Temporarily Unset Destination to stop directions
+        # TODO: maybe use an internal flag?
+        destination = get_destination()
+        update_destination_location(None)
 
-    print("Landmark", landmark)
-    update_destination_location(destination)
-    return {"message": "Nearest landmark found: '" + str(landmark) + "' with sequence: '" + str(sequence) + "'."}
+        # Get user's location
+        x, y = localisation.get_user_position()
+        landmark, sequence, direction, distance = find_nearest_landmark(x, y)
+
+        if (landmark == None):
+            # If no landmark is found inform user.
+            play_ack_sequence()
+            return {"message": "Nearest landmark found not found."}
+        else:
+            play_entered_sequence(sequence)
+            haptic_output.play_direction(
+                direction, 2, 0.1 * round(distance, -1), 5)
+
+        print("Landmark", landmark)
+        update_destination_location(destination)
+        return {"message": "Nearest landmark found: '" + str(landmark) + "' with sequence: '" + str(sequence) + "'."}
+
+    except Exception as e:
+        error()
+        return {"message": "Error Occured" + str(e)}
 
 
 @app.get("/setdestination/{sequence}")
 def update_destination(sequence):
-    destination = get_location_from_sequence(format_sequence_int(sequence))
-    if destination:
-        update_destination_location(destination)
-        return {"message": "destination updated to: " + str(destination)}
-    else:
-        haptic_output.play_effect(error_effect_id, 0.5, 2)
-        return {"message": "Sequence received is not mapped to a location"}
+    try:
+        destination = get_location_from_sequence(format_sequence_int(sequence))
+        if destination:
+            update_destination_location(destination)
+            return {"message": "destination updated to: " + str(destination)}
+        else:
+            error()
+            return {"message": "Sequence received is not mapped to a location"}
+    except Exception as e:
+        error()
+        return {"message": "Error Occured" + str(e)}
 
 
 @app.get("/update")
 def update():
-    destination = get_destination()
-    destination = (22 - round(destination[1]), round(destination[0]))
-    print(destination)
-    if (not destination):
-        return
-    x, y, h = localisation.get_user_location()
-    print("location", x, y, h)
-    next_direction, destination_reached = path_planner.calculate_next_direction(
-        (22 - round(y), round(x)), destination, 360 - h, 20, True, True)
+    try:
+        # Get the destination
+        destination = get_destination()
+        # TODO: Modify this to get dimensions from map (maybe store map dimensions as a property)
+        destination = (22 - round(destination[1]), round(destination[0]))
+        if (not destination):
+            return {"message": "No destination set doing nothing."}
 
-    if (destination_reached):
-        print("Destination Reached")
-        play_ack_sequence()
-        update_destination_location(None)
-        return
-    else:
-        if (next_direction):
-            print("Next Direction", next_direction)
-            haptic_output.play_direction(next_direction)
+        x, y, h = localisation.get_user_location()
+
+        # TODO: Modify this to get dimensions from map (maybe store map dimensions as a property)
+        # TODO: Figure out how offset can be made consistent
+        next_direction, destination_reached = path_planner.calculate_next_direction(
+            (22 - round(y), round(x)), destination, 360 - h, 20, True, True)
+
+        if (destination_reached):
+            print("Destination Reached")
+            play_ack_sequence()
+            update_destination_location(None)
+            return
         else:
-            pass
+            if (next_direction):
+                print("Next Direction", next_direction)
+                haptic_output.play_direction(next_direction)
+            else:
+                return {"message": "No Next Direction found."}
 
-
-@app.get("/testupdate")
-def testupdate():
-    destination = get_destination()
-    destination = (22 - round(destination[1]), round(destination[0]))
-    print(destination)
-    if (not destination):
-        return
-    x, y, h = localisation.get_user_location()
-    print("location", x, y, h)
-    next_direction, destination_reached = path_planner.calculate_next_direction(
-        (22 - round(y), round(x)), destination, 360 - h, 20, True, True)
-
-    if (destination_reached):
-        print("Destination Reached")
-        play_ack_sequence()
-        update_destination_location(None)
-        add_to_user_path("-------------Destination Reached-------------------")
-        return
-    else:
-        if (next_direction):
-            print("Next Direction", next_direction)
-            haptic_output.play_direction(next_direction)
-            add_to_user_path(str((x, y, 360 - h + 28)))
-        else:
-            pass
-
-# @app.get("/testupdate")
-# def test_update():
-#     start_time = time.time()
-#     destination = get_destination()
-#     destination = (22 - int(destination[1]*2), int(destination[0]*2))
-
-#     if (not destination):
-#         return
-
-#     a, b, c = localisation.get_user_location()
-
-#     start = free_points[random.randint(0, len(free_points))]
-#     end = free_points[random.randint(0, len(free_points))]
-#     h = random.randint(0, 360)
-
-#     next_direction, destination_reached = path_planner.calculate_next_direction(
-#         start, end, h, 0, False, False)
-
-#     end_time = time.time()
-#     elapsed_time = end_time - start_time
-#     add_execution_time(elapsed_time, start, end)
-
-#     if (destination_reached):
-#         # print("Destination Reached")
-#         # play_ack_sequence()
-#         # update_destination_location(None)
-#         return
-#     else:
-#         if (next_direction):
-#             pass
-#             # print("Next Direction", next_direction)
-#             # haptic_output.play_direction(next_direction)
-#         else:
-#             pass
+    except Exception as e:
+        error()
+        return {"message": "Error Occured" + str(e)}
