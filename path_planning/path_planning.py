f96ffc5ac59a39cadadb6b79e419e7e332e66057
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-06-30 12:37:18 +0100

    FIx path planning script

--- path_planning/path_planning.py
+++ path_planning/path_planning.py
@@ -44,6 +44,11 @@ class PathPlanner:
         map, node_density = load_map(self.map_name)
         self.map = map
         self.node_density = node_density
+        self.map_height = len(map)
+        self.map_width = len(map[0])
+
+        if not self.prev_path:
+            self.prev_path = []
 
     def change_map(self, new_map):
         self.map = new_map
@@ -56,129 +61,134 @@ class PathPlanner:
         :param end:
         :return:
         """
+        try:
+            # Create start and end node
+            # If start is an obstacle, set start to nearest open node
+            if self.map[start[0]][start[1]] != 0:
+                # print("invalid start node")
+                start = self.find_valid_start(start)
 
-        # Create start and end node
-        # If start is an obstacle, set start to nearest open node
-        if self.map[start[0]][start[1]] != 0:
-            # print("invalid start node")
-            start = self.find_valid_start(start)
+            start_node = Node(None, start)
+            start_node.g = start_node.h = start_node.f = 0
 
-        start_node = Node(None, start)
-        start_node.g = start_node.h = start_node.f = 0
+            # If end is an obstacle, set end to nearest open node
+            if self.map[end[0]][end[1]] != 0:
+                # print("invalid end node")
+                end = self.find_closest_open_node(end)
 
-        # If end is an obstacle, set end to nearest open node
-        if self.map[end[0]][end[1]] != 0:
-            # print("invalid end node")
-            end = self.find_closest_open_node(end)
+            end_node = Node(None, end)
+            end_node.g = end_node.h = end_node.f = 0
 
-        end_node = Node(None, end)
-        end_node.g = end_node.h = end_node.f = 0
+            # Initialize both open and closed list
+            open_list = []
+            closed_list = []
 
-        # Initialize both open and closed list
-        open_list = []
-        closed_list = []
+            # heapify the open_list and Add the start node
+            heapq.heapify(open_list)
+            heapq.heappush(open_list, start_node)
 
-        # heapify the open_list and Add the start node
-        heapq.heapify(open_list)
-        heapq.heappush(open_list, start_node)
+            # Adding a stop condition
+            outer_iterations = 0
+            max_iterations = 20000  # (len(self.map[0]) * len(self.map) // 2)
 
-        # Adding a stop condition
-        outer_iterations = 0
-        max_iterations = 20000  # (len(self.map[0]) * len(self.map) // 2)
+            # what squares do we search
+            adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
+            if allow_diagonal_movement:
+                adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),
+                                    (-1, -1), (-1, 1), (1, -1), (1, 1),)
 
-        # what squares do we search
-        adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
-        if allow_diagonal_movement:
-            adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),
-                                (-1, -1), (-1, 1), (1, -1), (1, 1),)
+            # Loop until you find the end
+            while len(open_list) > 0:
+                outer_iterations += 1
 
-        # Loop until you find the end
-        while len(open_list) > 0:
-            outer_iterations += 1
+                if outer_iterations > max_iterations:
+                    # if we hit this point return the path such as it is
+                    # it will not contain the destination
+                    warn("giving up on pathfinding too many iterations")
+                    return self.__return_path(current_node)
 
-            if outer_iterations > max_iterations:
-                # if we hit this point return the path such as it is
-                # it will not contain the destination
-                warn("giving up on pathfinding too many iterations")
-                return self.__return_path(current_node)
+                # Get the current node
+                current_node = heapq.heappop(open_list)
+                closed_list.append(current_node)
 
-            # Get the current node
-            current_node = heapq.heappop(open_list)
-            closed_list.append(current_node)
+                # Found the goal
+                if current_node == end_node:
+                    return self.__return_path(current_node)
 
-            # Found the goal
-            if current_node == end_node:
-                return self.__return_path(current_node)
+                # Generate children
+                children = []
 
-            # Generate children
-            children = []
+                for new_position in adjacent_squares:  # Adjacent squares
 
-            for new_position in adjacent_squares:  # Adjacent squares
+                    # Get node position
+                    node_position = (
+                        current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
-                # Get node position
-                node_position = (
-                    current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-
-                # Make sure within range
-                if node_position[0] > (len(self.map) - 1) or node_position[0] < 0 or node_position[1] > (len(self.map[0]) - 1) or node_position[1] < 0:
-                    continue
-
-                # Make sure walkable terrain
-                if self.map[node_position[0]][node_position[1]] != 0:
-                    continue
-
-                # Check diagonal movement is valid (i.e. they don't need to walk through a wall)
-                # Diagonal move
-                if abs(new_position[0]) - abs(node_position[0]) != 0 and abs(new_position[1]) - abs(node_position[1]) != 0:
-                    left_node_x = node_position[0] - new_position[0]
-                    left_node_y = node_position[1]
-                    right_node_x = node_position[0]
-                    right_node_y = node_position[1] - new_position[1]
-
-                    # Check validity of nodes next to diagnonal
-                    if self.map[left_node_x][left_node_y] != 0 or self.map[right_node_x][right_node_y] != 0:
+                    # Make sure within range
+                    if node_position[0] > (len(self.map) - 1) or node_position[0] < 0 or node_position[1] > (len(self.map[0]) - 1) or node_position[1] < 0:
                         continue
 
-                # Create new node
-                new_node = Node(current_node, node_position)
+                    # Make sure walkable terrain
+                    if self.map[node_position[0]][node_position[1]] != 0:
+                        continue
 
-                # Append
-                children.append(new_node)
+                    # Check diagonal movement is valid (i.e. they don't need to walk through a wall)
+                    # Diagonal move
+                    if abs(new_position[0]) - abs(node_position[0]) != 0 and abs(new_position[1]) - abs(node_position[1]) != 0:
+                        left_node_x = node_position[0] - new_position[0]
+                        left_node_y = node_position[1]
+                        right_node_x = node_position[0]
+                        right_node_y = node_position[1] - new_position[1]
 
-            # Loop through children
-            for child in children:
+                        # Check validity of nodes next to diagnonal
+                        if self.map[left_node_x][left_node_y] != 0 or self.map[right_node_x][right_node_y] != 0:
+                            continue
 
-                # Child is on the closed list
-                if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
-                    continue
+                    # Create new node
+                    new_node = Node(current_node, node_position)
 
-                # Create the g, h and f values
-                child.h = (((child.position[0] - end_node.position[0]) ** 2) + (
-                    (child.position[1] - end_node.position[1]) ** 2)) ** 0.5
+                    # Append
+                    children.append(new_node)
 
-                # Diagonal move
-                if abs(child.position[0]) - abs(child.parent.position[0]) != 0 and abs(child.position[1]) - abs(child.parent.position[1]) != 0:
-                    child.g = current_node.g + 1.414
-                else:
-                    child.g = current_node.g + 1
+                # Loop through children
+                for child in children:
 
-                for cell in self.get_cell_radius(child.position[0], child.position[1]):
-                    try:
-                        if self.map[cell[0]][cell[1]] != 0:
-                            child.g += -0.2 if hug_objects else 0.2
-                    except:
-                        pass
+                    # Child is on the closed list
+                    if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
+                        continue
 
-                child.f += child.g + child.h
+                    # Create the g, h and f values
+                    child.h = (((child.position[0] - end_node.position[0]) ** 2) + (
+                        (child.position[1] - end_node.position[1]) ** 2)) ** 0.5
 
-                # Child is already in the open list
-                if len([open_node for open_node in sorted(open_list) if child.position == open_node.position and child.f >= open_node.f]) > 0:
-                    continue
+                    # Diagonal move
+                    if abs(child.position[0]) - abs(child.parent.position[0]) != 0 and abs(child.position[1]) - abs(child.parent.position[1]) != 0:
+                        child.g = current_node.g + 1.414
+                    else:
+                        child.g = current_node.g + 1
 
-                # Add the child to the open list
-                heapq.heappush(open_list, child)
+                    for cell in self.get_cell_radius(child.position[0], child.position[1]):
+                        try:
+                            if self.map[cell[0]][cell[1]] != 0:
+                                child.g += -0.2 if hug_objects else 0.2
+                        except:
+                            pass
 
-        warn("Couldn't get a path to destination")
+                    child.f += child.g + child.h
+
+                    # Child is already in the open list
+                    if len([open_node for open_node in sorted(open_list) if child.position == open_node.position and child.f >= open_node.f]) > 0:
+                        continue
+
+                    # Add the child to the open list
+                    heapq.heappush(open_list, child)
+            
+        except Exception as e:
+            print(e)
+            t = e.__traceback__
+            print(t.tb_lineno)
+
+        print("Couldn't get a path to destination")
         return None
 
     def get_cell_radius(self, x, y, distance=1):
@@ -231,7 +241,7 @@ class PathPlanner:
             current = current.parent
         return path[::-1]
 
-    def __get_target_heading(self, node1, node2):
+    def get_target_heading(self, node1, node2):
         # 0 degrees is toward right on printed map
         # +1 y is downwards
         # +1 x is towards right
@@ -239,24 +249,26 @@ class PathPlanner:
         # print(node1, node2)
         required_movement_direction_y = (node2[0] - node1[0])
         required_movement_direction_x = (node2[1] - node1[1])
-        # print(required_movement_direction_y, required_movement_direction_x)
+        print(required_movement_direction_x, required_movement_direction_y)
+
+        target_heading  = 0
 
         if required_movement_direction_x == 0 and required_movement_direction_y >= 1:
-            target_heading = 270
-        elif required_movement_direction_x >= 1 and required_movement_direction_y >= 1:
-            target_heading = 315
-        elif required_movement_direction_x >= 1 and required_movement_direction_y == 0:
-            target_heading = 0
-        elif required_movement_direction_x >= 1 and required_movement_direction_y <= -1:
-            target_heading = 45
-        elif required_movement_direction_x == 0 and required_movement_direction_y <= -1:
-            target_heading = 90
-        elif required_movement_direction_x <= -1 and required_movement_direction_y <= -1:
-            target_heading = 135
-        elif required_movement_direction_x <= -1 and required_movement_direction_y == 0:
             target_heading = 180
-        elif required_movement_direction_x <= -1 and required_movement_direction_y >= 1:
+        elif required_movement_direction_x >= 1 and required_movement_direction_y >= 1:
             target_heading = 225
+        elif required_movement_direction_x >= 1 and required_movement_direction_y == 0:
+            target_heading = 270
+        elif required_movement_direction_x >= 1 and required_movement_direction_y <= -1:
+            target_heading = 315
+        elif required_movement_direction_x == 0 and required_movement_direction_y <= -1:
+            target_heading = 0
+        elif required_movement_direction_x <= -1 and required_movement_direction_y <= -1:
+            target_heading = 45
+        elif required_movement_direction_x <= -1 and required_movement_direction_y == 0:
+            target_heading = 90
+        elif required_movement_direction_x <= -1 and required_movement_direction_y >= 1:
+            target_heading = 135
 
         return target_heading
 
@@ -284,7 +296,7 @@ class PathPlanner:
                     line.append(" L ")
             print("".join(line))
 
-    def __map_angle_to_direction(self, heading_change):
+    def map_angle_to_direction(self, heading_change):
         heading_change = round(8*(heading_change)/360, 0)
         # print("heading change", heading_change)
 
@@ -316,63 +328,72 @@ class PathPlanner:
     def __translate_node(self, node):
         x_origin, y_origin = (0, 0)
         translated_x = ((node[1] + 1)*self.node_density)-x_origin
-        translated_y = ((len(self.map)-node[0])*self.node_density)-y_origin
+        translated_y = ((self.map_height - node[0])*self.node_density)-y_origin
         translated_node = (translated_x, translated_y)
         return translated_node
 
     def calculate_next_direction(self, start, end, heading, offset, print_map=False, print_path=False, hug_objects=False):
-        self.refresh_map()
-        destination_reached = False
+        try:
+            self.refresh_map()
+            destination_reached = False
 
-        print("start", start)
-        print("end", end)
-        print("heading", heading)
-        print("offset", offset)
+            start = tuple((self.map_height - start[0], start[1]))
+            end = tuple((self.map_height - end[0], end[1]))
 
-        # If user is within one cell of destination
-        if end in self.get_cell_radius(start[0], start[1]):
-            # User has arrived
-            destination_reached = True
-            return (None, destination_reached)
-
-        # Determine Optimal Path
-        path = self.astar(start, end, hug_objects)
-        self.prev_path = path
-
-        if path:
-            # Determine Required Heading
-            # if user is not at start (due to start being obstacle)
-            if start != path[0]:
-                # required heading is towards start
-                required_heading = self.__get_target_heading(start, path[0])
-            else:
-                # else required heading is towards next step
-                required_heading = self.__get_target_heading(path[0], path[1])
-
-            # Apply offset to current heading
-            heading = heading + offset
-            if heading >= 360:
-                heading -= 360
-
-            # Calculate change in heading required
-            heading_change = required_heading - heading
+            print("start", start)
+            print("end", end)
             print("heading", heading)
-            print("required_heading", required_heading)
-            print("heading_change", heading_change)
+            print("offset", offset)
 
-            # Map change in heading to direction
-            turn_direction = self.__map_angle_to_direction(heading_change)
+            # If user is within one cell of destination
+            neighbours = self.get_cell_radius(start[0], start[1])
+            if end in neighbours or end == start:
+                # User has arrived
+                destination_reached = True
+                return (None, destination_reached)
 
-            if print_map:
-                self.__print_map(path, path[0], path[-1])
+            # Determine Optimal Path
+            path = self.astar(start, end, hug_objects)
+            self.prev_path = path
 
-            if print_path:
-                # Translate path back from nodes to location
-                print(self.__translate_path(path))
+            if path:
+                # Determine Required Heading
+                # if user is not at start (due to start being obstacle)
+                if start != path[0]:
+                    # required heading is towards start
+                    required_heading = self.get_target_heading(start, path[0])
+                else:
+                    # else required heading is towards next step
+                    required_heading = self.get_target_heading(path[0], path[1])
 
-            return (turn_direction, destination_reached)
-        else:
-            return (False, False)
+                # Apply offset to current heading
+                heading = heading + offset
+                if heading >= 360:
+                    heading -= 360
+
+                # Calculate change in heading required
+                heading_change = required_heading - heading
+                print("heading", heading)
+                print("required_heading", required_heading)
+                print("heading_change", heading_change)
+
+                # Map change in heading to direction
+                turn_direction = self.map_angle_to_direction(heading_change)
+
+                if print_map:
+                    self.__print_map(path, path[0], path[-1])
+
+                if print_path:
+                    # Translate path back from nodes to location
+                    print(self.__translate_path(path))
+
+                return (turn_direction, destination_reached)
+            else:
+                return (False, False)
+        except Exception as e:
+            print(e)
+            t = e.__traceback__
+            print(t.tb_lineno)
 
 
 # Usage Example
