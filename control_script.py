01c42e4ebfc0fa549c1044eb4e2f905a2ea55787
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-06-30 12:31:11 +0100

    Reduce control script frequency to 1Hz

--- control_script.py
+++ control_script.py
@@ -1,8 +1,8 @@
 import requests
 from time import sleep
-url = "http://192.168.137.13:8000/testupdate"
+url = "http://192.168.137.13:8000/update"
 
 while True:
     print("Sending Request")
     requests.get(url)
-    sleep(0.5)  # 2Hz
+    sleep(1)  # 1Hz
