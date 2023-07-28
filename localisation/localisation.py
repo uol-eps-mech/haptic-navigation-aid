ab26e3f7620c5057bed4df8020443408098f64b6
Kaif-Kutchwala <kaifkutchwala@gmail.com>
2023-07-28 14:40:58 +0100

    Add comments to localisation scripts

--- localisation/localisation.py
+++ localisation/localisation.py
@@ -12,26 +12,34 @@ from dwm1001_apiCommands import DWM1001_API_COMMANDS
 
 class Localisation:
     def __init__(self, i2c):
+        # Initialize the Localisation class with the provided I2C bus
         self.sensor = adafruit_bno055.BNO055_I2C(i2c)
+
+        # Set the permission of the serial port '/dev/ttyACM0' to allow access
         os.popen("sudo chmod 777 /dev/ttyACM0", "w")
 
+        # Create a serial port object for communicating with the DWM1001 device
         self.serialPortDwm1001 = serial.Serial(port="/dev/ttyACM0",
                                                baudrate=115200,
                                                parity=SYS_DEFS.parity,
                                                stopbits=SYS_DEFS.stopbits,
                                                bytesize=SYS_DEFS.bytesize)
-        #TODO: Fix BNO055 callibration
+
+        # TODO: Fix BNO055 calibration
         # self.setup_bno055()
+        # Initialize the DWM1001 API and establish a connection
         self.initializeDWM1001API()
         self.connectDWM1001()
 
     def get_user_heading(self):
+        # Get the user's heading by reading the Euler angle from the BNO055 sensor
         for _ in range(5):
             heading = self.sensor.euler[0]  # - default_heading
         print(heading)
         return heading
 
     def setup_bno055(self):
+        # Setup the BNO055 sensor for calibration
         self.sensor.mode = adafruit_bno055.CONFIG_MODE
         time.sleep(0.2)
         self.sensor.offsets_accelerometer = (-29, 21, -28)
@@ -43,7 +51,7 @@ class Localisation:
         time.sleep(0.2)
 
     def check_bno055_calibrated(self):
-        # Check if the sensor is calibrated. If not, keep checking.
+        # TODO: Check if the BNO055 sensor is calibrated. If not, keep checking.
         while not self.sensor.calibrated:
             print("Sensor not calibrated yet. Please follow the calibration process.")
             print("Current Status:", self.sensor.calibration_status)
@@ -53,6 +61,7 @@ class Localisation:
         print(self.get_bno055_offsets())
 
     def get_bno055_offsets(self):
+        # TODO: Get the offsets and radius values from the BNO055 sensor
         offsets = {
             "accelerometer": self.sensor.offsets_accelerometer,
             "gyroscope": self.sensor.offsets_gyroscope,
@@ -63,9 +72,9 @@ class Localisation:
         return offsets
 
     def initializeDWM1001API(self):
-        # reset incase previuos run didn't close properly
+        # Reset the DWM1001 in case a previous run didn't close properly
         self.serialPortDwm1001.write(DWM1001_API_COMMANDS.RESET)
-        # send ENTER two times in order to access api
+        # Send ENTER two times in order to access the DWM1001 API
         self.serialPortDwm1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
         time.sleep(0.5)
         self.serialPortDwm1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
@@ -73,28 +82,33 @@ class Localisation:
         self.serialPortDwm1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
 
     def connectDWM1001(self):
+        # Close and reopen the serial port to establish a connection with the DWM1001
         self.serialPortDwm1001.close()
         time.sleep(0.2)
         self.serialPortDwm1001.open()
         time.sleep(0.5)
-        if (self.serialPortDwm1001.isOpen()):
+
+        # Check if the serial port is open and send the LEC command to the DWM1001
+        if self.serialPortDwm1001.isOpen():
             time.sleep(0.5)
             self.serialPortDwm1001.write(DWM1001_API_COMMANDS.LEC)
             self.serialPortDwm1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
             time.sleep(0.5)
 
     def reset_buffer(self):
+        # Reset the input and output buffers of the serial port
         self.serialPortDwm1001.reset_input_buffer()
         self.serialPortDwm1001.reset_output_buffer()
         time.sleep(0.1)
 
     def get_user_position(self):
         global serialReadLine
-        if (self.serialPortDwm1001.isOpen()):
+        if self.serialPortDwm1001.isOpen():
             self.reset_buffer()
             self.serialPortDwm1001.write(DWM1001_API_COMMANDS.APG)
             location_data = []
             while len(location_data) < 1 or location_data[0] != 'POS':
+                # Read data from the serial port until a valid location data is received
                 serialReadLine = self.serialPortDwm1001.read_until()
                 # print(serialReadLine)
                 location_data = str(serialReadLine, 'utf-8').split(',')
@@ -102,20 +116,21 @@ class Localisation:
             y_pos = float(location_data[4])
             coordinates = [x_pos, y_pos]
             return coordinates
-
         else:
             print("Can't open port: " + str(self.serialPortDwm1001.name))
             return False
 
     def get_user_location(self):
+        # Get the user's position and heading and return them as a tuple (x, y, heading)
         user_position = self.get_user_position()
         heading = self.get_user_heading()
         if user_position:
-            return (user_position[0], user_position[1], heading)  # x, y, h
+            return (user_position[0], user_position[1], heading)
         return False
 
 
 def test():
+    # Test function to get user location using the Localisation class
     localisation = Localisation(board.I2C())
     while True:
         location = localisation.get_user_location()
@@ -123,10 +138,9 @@ def test():
 
 
 def test_dwm():
+    # Test function to get user position using the Localisation class with TCA9548A multiplexer
     i2c = board.I2C()
-
     i2cExpander = adafruit_tca9548a.TCA9548A(i2c)
-
     localisation = Localisation(i2cExpander[6])
     while True:
         location = localisation.get_user_position()
--- localisation/localisationv2.py
+++ localisation/localisationv2.py
@@ -8,28 +8,37 @@ import json
 
 class Localisation:
     def __init__(self):
-        print("Reading COM Ports")
+        print("Reading COM Ports:")
+        
+        # Use subprocess to list available COM ports
         s = subprocess.getstatusoutput(f'python -m serial.tools.list_ports')
         print(s[1])
 
+        # Prompt the user to input the COM port for the DWM1001 device
         self.com_port = input("Please enter the COM port to use for DWM1001: ")
 
-        # Replace with the name of your serial port
+        # Define the serial port using the provided COM port number
         self.serial_port = 'COM' + str(self.com_port)
+        
         # Set the baud rate to match the configuration of the DWM1001 listener
         self.baud_rate = 115200
+
+        # Create a serial port object for communicating with the DWM1001 device
         self.serialPortDwm1001 = serial.Serial(
             self.serial_port, self.baud_rate)
 
+        # Set the URL for the server to get the user's heading
         self.url = "http://192.168.2.122:8000/getheading"
 
+        # Initialize the DWM1001 API commands and connect to the device
         self.initializeDWM1001API()
         self.connectDWM1001()
 
     def initializeDWM1001API(self):
-        # reset incase previuos run didn't close properly
+        # Reset the DWM1001 in case a previous run didn't close properly
         self.serialPortDwm1001.write(DWM1001_API_COMMANDS.RESET)
-        # send ENTER two times in order to access api
+
+        # Send ENTER two times in order to access the DWM1001 API
         self.serialPortDwm1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
         time.sleep(0.5)
         self.serialPortDwm1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
@@ -37,22 +46,27 @@ class Localisation:
         self.serialPortDwm1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
 
     def connectDWM1001(self):
+        # Close and reopen the serial port to establish a connection with the DWM1001
         self.serialPortDwm1001.close()
         time.sleep(0.2)
         self.serialPortDwm1001.open()
         time.sleep(0.5)
-        if (self.serialPortDwm1001.isOpen()):
+
+        # Check if the serial port is open and send the LEC command to the DWM1001
+        if self.serialPortDwm1001.isOpen():
             time.sleep(0.5)
             self.serialPortDwm1001.write(DWM1001_API_COMMANDS.LEC)
             self.serialPortDwm1001.write(DWM1001_API_COMMANDS.SINGLE_ENTER)
             time.sleep(0.5)
 
     def reset_buffer(self):
+        # Reset the input and output buffers of the serial port
         self.serialPortDwm1001.reset_input_buffer()
         self.serialPortDwm1001.reset_output_buffer()
         time.sleep(0.1)
 
     def get_user_heading(self):
+        # Send a request to the server to get the user's heading and parse the response
         for _ in range(2):
             response = requests.get(self.url)
         print(response.content)
@@ -62,10 +76,11 @@ class Localisation:
 
     def get_user_position(self):
         global serialReadLine
-        if (self.serialPortDwm1001.isOpen()):
+        if self.serialPortDwm1001.isOpen():
             self.reset_buffer()
             location_data = []
             while len(location_data) < 1 or location_data[0] != 'POS':
+                # Read data from the serial port until a valid location data is received
                 serialReadLine = self.serialPortDwm1001.read_until()
                 location_data = str(serialReadLine, 'utf-8').split(',')
                 # print(location_data, location_data[0] != 'POS')
@@ -73,14 +88,14 @@ class Localisation:
             y_pos = float(location_data[4])
             coordinates = [x_pos, y_pos]
             return coordinates
-
         else:
             print("Can't open port: " + str(self.serialPortDwm1001.name))
             return False
 
     def get_user_location(self):
+        # Get the user's position and heading and return them as a tuple (x, y, h)
         user_position = self.get_user_position()
         heading = self.get_user_heading()
         if user_position:
-            return (user_position[0], user_position[1], heading)  # x, y, h
+            return (user_position[0], user_position[1], heading)
         return False
